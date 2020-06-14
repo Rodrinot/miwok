f8be25711707449bacbd9a69384f4372a7228b1e
Rodrigo Notario <rodrinot@ucm.es>
2020-06-14 20:10:30 +0200

    LinearLayout changed to ListView.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,14 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,5 +18,14 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an array of words.
+        ArrayList<String> words = new ArrayList<String> (Arrays.asList("one", "two", "three",
+                "four", "five", "six", "seven", "eight", "nine", "ten"));
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
