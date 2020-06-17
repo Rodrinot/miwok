afd64d655529dc16680d3ef77fe67ada6680c8ba
Rodrigo Notario <rodrinot@ucm.es>
2020-06-17 16:07:38 +0200

    Add images.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.widget.ImageView;
+
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
  * It contains a default translation and a Miwok translation for that word.
@@ -12,6 +14,9 @@ public class Word {
     /** Miwok translation for the word */
     private String mMiwokTranslation;
 
+    /** Image resource id */
+    private int mResourceImageId;
+
     /**
      * Create a new Word object.
      *
@@ -22,6 +27,19 @@ public class Word {
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Create a new Word object with image.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int resourceImageId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mResourceImageId = resourceImageId;
     }
 
     /**
@@ -37,4 +55,10 @@ public class Word {
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+
+    /**
+     * Get the resource image id of the word.
+     */
+    public int getImageResourceId() { return mResourceImageId; }
+
 }
